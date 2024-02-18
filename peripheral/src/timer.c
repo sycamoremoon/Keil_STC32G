72d252227a212b9a6df09c3dc2b72b6e7fb4df82
wanggood <wanggood202207@163.com>
2024-02-18 13:34:16 +0800

    delete timer.c some codes

--- peripheral/src/timer.c
+++ peripheral/src/timer.c
@@ -39,19 +39,6 @@
 //-------------------------------------------------------------------------------------------------------------------
 void ctimer_count_init(CTIMN_enum tim_n)
 {
-<<<<<<< HEAD
-	TIM_InitTypeDef		TIM_InitStructure;					//结构定义
-	TIM_InitStructure.TIM_Mode      = TIM_16BitAutoReload;	//指定工作模式,   TIM_16BitAutoReload,TIM_16Bit,TIM_8BitAutoReload,TIM_16BitAutoReloadNoMask
-	TIM_InitStructure.TIM_ClkSource = TIM_CLOCK_1T;			//指定时钟源,     TIM_CLOCK_1T,TIM_CLOCK_12T,TIM_CLOCK_Ext
-	TIM_InitStructure.TIM_ClkOut    = DISABLE;				//是否输出高速脉冲, ENABLE或DISABLE
-	TIM_InitStructure.TIM_Value     = 0 ;					//初值,
-	TIM_InitStructure.TIM_PS        = 0;					//8位预分频器(n+1), 0~255
-	TIM_InitStructure.TIM_Run       = ENABLE;				//是否初始化后启动定时器, ENABLE或DISABLE
-	Timer_Inilize(Timer0,&TIM_InitStructure);				//初始化Timer0	  Timer0,Timer1,Timer2,Timer3,Timer4
-//	NVIC_Timer0_Init(ENABLE,Priority_0);					//中断使能, ENABLE/DISABLE; 优先级(低到高) Priority_0,Priority_1,Priority_2,Priority_3
-=======
->>>>>>> 992b7b0ce695b66f653b05adfb413c147237c1c7
-
 	switch(tim_n)
 	{
 		case CTIM0_P34:
